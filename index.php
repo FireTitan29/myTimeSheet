92125b6e549e30c5dd523b357158a73cdacd3686
Tyler Duggan <tylersduggan@gmail.com>
2026-01-14 11:56:44 +0200

    CSS Force Reload

--- index.php
+++ index.php
@@ -65,9 +65,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover">
     <title>Healthy Pet: <?= $page ?></title>
-    <link rel="stylesheet" href="mystyle.css?v=12">
+    <link rel="stylesheet" href="mystyle.css?v=13">
     <?php if (isset( $_SESSION['admin'])): ?>
-        <link rel="stylesheet" href="media-queries.css?v=4">
+        <link rel="stylesheet" href="media-queries.css?v=5">
     <?php endif; ?>
     <link rel="icon" type="image/x-icon" href="images/icons/staff_management_icon.svg">
 </head>
